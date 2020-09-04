42f469835982ae766119d2c4a979ad6b72f08694
Anderson Sathler <asathler@gmail.com>
2020-09-04 03:07:48 -0300

    gitflow-feature-stash: 02-show-languages-icons

--- resources/views/vendor/backpack/base/inc/topbar_right_content.blade.php
+++ resources/views/vendor/backpack/base/inc/topbar_right_content.blade.php
@@ -1,6 +1,26 @@
+{{-- flag-icon-css --}}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.3.0/css/flag-icon.min.css">
+
 <!-- This file is used to store topbar (right) items -->
 
+<li class="nav-item d-md-down-none">
+    <a class="nav-link" href="#">
+        <i class="la la-bell"></i>
+        <span class="badge badge-pill badge-danger">5</span>
+    </a>
+</li>
 
-{{-- <li class="nav-item d-md-down-none"><a class="nav-link" href="#"><i class="la la-bell"></i><span class="badge badge-pill badge-danger">5</span></a></li>
-<li class="nav-item d-md-down-none"><a class="nav-link" href="#"><i class="la la-list"></i></a></li>
-<li class="nav-item d-md-down-none"><a class="nav-link" href="#"><i class="la la-map"></i></a></li> --}}
+<li class="nav-item d-md-down-none">
+    <a class="nav-link" href="#">
+        <i class="la la-list"></i>
+    </a>
+</li>
+
+<li class="nav-item d-md-down-none">
+    <a class="nav-link" href="#">
+        <i class="la la-map"></i>
+    </a>
+</li>
+
+@include('backpack::inc.dropdowns.languages')
+
