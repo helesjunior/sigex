9f42107be8bdb54679d1c2b0525137a5c373f9ab
Heles <helesjunior@gmail.com>
2020-09-03 21:58:40 -0300

    Alter Backpack colors

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,17 @@
 
 namespace App\Models;
 
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use CrudTrait;
+    use HasRoles;
 
 
     protected $fillable = [
