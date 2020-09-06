add3fbdc7524d331ff8c435654db5a12253698a7
Anderson Sathler <asathler@gmail.com>
2020-09-06 05:36:55 -0300

    minor

--- resources/lang/vendor/backpack/pt_br/menu.php
+++ resources/lang/vendor/backpack/pt_br/menu.php
@@ -4,7 +4,7 @@ return [
 
     'dashboard'              => 'Dashboard',
     'users'                  => 'Usuários',
-    'settings'               => 'Confirgurações',
+    'settings'               => 'Configurações',
     'backups'                => 'Backups',
     'logs'                   => 'Registros',
     'authentication'         => 'Autenticação',
