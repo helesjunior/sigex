497105e8495918a9279a24db3f8e04af21d917a2
Anderson Sathler <asathler@gmail.com>
2020-09-06 05:34:07 -0300

    Backpack vendor publish --lang

---
+++ resources/lang/vendor/backpack/en/menu.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'dashboard'              => 'Dashboard',
+    'users'                  => 'Users',
+    'settings'               => 'Settings',
+    'backups'                => 'Backups',
+    'logs'                   => 'Logs',
+    'authentication'         => 'Authentication',
+    'roles'                  => 'Roles',
+    'permissions'            => 'Permissions',
+
+];
