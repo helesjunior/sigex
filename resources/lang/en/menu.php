4fb87990a2e7ba21b73be25109554c114e2b34d4
Anderson Sathler <asathler@gmail.com>
2020-09-06 22:50:27 -0300

    gitflow-feature-stash: 05-final-tunning-in-lang

---
+++ resources/lang/en/menu.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'authentication'         => 'Authentication',
+    'backups'                => 'Backups',
+    'dashboard'              => 'Dashboard',
+    'logs'                   => 'Logs',
+    'permissions'            => 'Permissions',
+    'roles'                  => 'Roles',
+    'settings'               => 'Settings',
+    'users'                  => 'Users',
+
+];
