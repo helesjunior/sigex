851bafd713eb2df37db8a0c9439ee168752d16d3
Anderson Sathler <asathler@gmail.com>
2020-09-08 21:23:10 -0300

    partial commit

---
+++ database/migrations/2020_09_08_225957_create_codes_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('codes', function (Blueprint $table) {
+            $table->id()->comment("Table's unique identifier");
+            $table->string('description', 50)->comment('Brief general description of domain data');
+            $table->boolean('is_visible')->default(true)->comment('Showed or not');
+
+            // $table->timestamps();
+            $table->timestamp('created_at')->nullable()->comment('Creation date and time');
+            $table->timestamp('updated_at')->nullable()->comment('Last update date and time');
+
+            // $table->softDeletes();
+            $table->timestamp('deleted_at')->nullable()->comment('Deletion date and time');
+        });
+
+        DB::statement("COMMENT ON TABLE codes IS
+            'All domain data with id, description and is_visible are stored here'
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('codes');
+    }
+}
