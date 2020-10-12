f31930d05a56a7f70c8a34c68999f13a1b27e774
Anderson Sathler <asathler@gmail.com>
2020-10-12 14:48:38 -0300

    Country (quick one)

---
+++ database/migrations/2020_10_12_174135_create_countries_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id()->comment("Table's unique identifier");
+            $table->string('name')->comment('Country name');
+            $table->boolean('status')->comment('Active or inactive status');
+
+            // $table->timestamps();
+            $table->timestamp('created_at')->nullable()->comment('Creation date and time');
+            $table->timestamp('updated_at')->nullable()->comment('Last update date and time');
+
+            $table->softDeletes()->comment('Deletion date and time');
+        });
+
+        DB::statement("COMMENT ON TABLE countries IS
+            '...'
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
