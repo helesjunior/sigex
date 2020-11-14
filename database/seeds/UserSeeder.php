3354720f1e91d70326e550b347306128e33f0aba
Anderson Sathler <asathler@gmail.com>
2020-11-14 20:36:20 -0300

    Inclusão do Saulo e Franklim (no seed)

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -28,5 +28,23 @@ class UserSeeder extends Seeder
             'remember_token' => 'iAVpRQt4S9',
             'language' => 'en'
         ]);
+
+        \App\Models\User::create([
+            'name' => 'Saulo Soares',
+            'email' => 'saulosao@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => 'Kwkvum4wJP',
+            'language' => 'en'
+        ]);
+
+        \App\Models\User::create([
+            'name' => 'Franklin Silva',
+            'email' => 'franklin.linux@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => '9S4tQRpVAi',
+            'language' => 'pt_br'
+        ]);
     }
 }
