393d1d23c589e9bd9a3b97682e959d0298adf1a8
Anderson Sathler <asathler@gmail.com>
2020-11-15 23:03:23 -0300

    View blade changes for new Aid\Menu

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,67 +1,8 @@
-<!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
-<li class="nav-item">
-    <a class="nav-link" href="{{ backpack_url('dashboard') }}">
-        <i class="la la-home nav-icon"></i>
-        {{ __('base.dashboard') }}
-    </a>
-</li>
+@php
+    $menu = new \App\Aid\Menu();
+    $mainMenu = $menu->makeMainMenu();
+@endphp
 
-<li class='nav-item'>
-    <a class='nav-link' href='{{ backpack_url('backup') }}'>
-        <i class='nav-icon la la-hdd-o'></i>
-        {{ __('menu.backups') }}
-    </a>
-</li>
-<li class='nav-item'>
-    <a class='nav-link' href='{{ backpack_url('log') }}'>
-        <i class='nav-icon la la-terminal'></i>
-        {{ __('menu.logs') }}
-    </a>
-</li>
-
-<!-- Users, Roles, Permissions -->
-<li class="nav-item nav-dropdown">
-    <a class="nav-link nav-dropdown-toggle" href="#">
-        <i class="nav-icon la la-users"></i>
-        {{ __('menu.authentication') }}
-    </a>
-    <ul class="nav-dropdown-items">
-        <li class="nav-item"
-        ><a class="nav-link" href="{{ backpack_url('user') }}">
-                <i class="nav-icon la la-user"></i>
-                <span>
-                    {{ __('menu.users') }}
-                </span>
-            </a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link" href="{{ backpack_url('role') }}">
-                <i class="nav-icon la la-id-badge"></i>
-                <span>
-                    {{ __('menu.roles') }}
-                </span>
-            </a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link" href="{{ backpack_url('permission') }}">
-                <i class="nav-icon la la-key"></i>
-                <span>
-                    {{ __('menu.permissions') }}
-                </span>
-            </a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link" href="{{ backpack_url('code') }}">
-                <i class="nav-icon la la-code"></i>
-                <span>
-                    {{ __('menu.code_items') }}
-                </span>
-            </a>
-        </li>
-    </ul>
-</li>
-
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('creditors') }}'><i class='nav-icon la la-question'></i> Creditors</a></li>
+@foreach($mainMenu as $item)
+    @include('backpack::menu.item')
+@endforeach
