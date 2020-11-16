64b2fcc0b7bd0cef0a43eec00f54c67b74f95172
Anderson Sathler <asathler@gmail.com>
2020-11-16 02:54:27 -0300

    Initial factory

--- database/factories/UnitFactory.php
+++ database/factories/UnitFactory.php
@@ -6,7 +6,20 @@ use App\Models\Unit;
 use Faker\Generator as Faker;
 
 $factory->define(Unit::class, function (Faker $faker) {
+    $name = $faker->company;
+
     return [
-        // campos a definir
+        'organ_id' => \App\Models\Organ::all()->random()->id,
+        'code' => $faker->randomNumber(6),
+        'name' => $name,
+        'short_name' => \Illuminate\Support\Str::of($name)->substr(0, 6)->upper(),
+        'phone' => $faker->phoneNumber(20),
+        'currency_id' => \App\Models\Currency::all()->random()->id,
+        'city_id' => null,
+        'state_id' => null,
+        'country_id' => null,
+        'type_id' => 0,
+        'status' => $faker->boolean,
+        // campos extras?
     ];
 });
